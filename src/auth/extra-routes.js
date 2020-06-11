ce3932189a52a9b78f7aff3d5283f20e2d44c824
Bushra-B <bushraxbil@gmail.com>
2020-06-11 10:45:47 +0300

    initializing lab 14

--- src/auth/extra-routes.js
+++ src/auth/extra-routes.js
@@ -8,5 +8,20 @@ const bearerMiddleware = require('./middleware/bearer');
 
 router.get('/secret', bearerMiddleware, (req,res) => {} );
 
+router.post('/add', bearerAuth, permissions('create'), (req,res)=>{
+  res.status(201).send('created ..');
+});
+  
+router.get('/read', bearerAuth, permissions('read'),(req,res)=>{
+  res.status(201).send('Allowed reading ');
+});
+  
+router.put('/change', bearerAuth, permissions('update'), (req,res)=>{
+  res.status(201).send('changed ..');
+});
+router.delete('/remove', bearerAuth, permissions('delete'),(req,res)=>{
+  res.status(201).send('removed  ..');
+});
+  
 
 module.exports = router;
